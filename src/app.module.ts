44f5fd4b9adcd6e2c172326cd0a39d160b2d4808
sasdaiv <sasdaiv@gmail.com>
2024-04-02 18:01:39 +0300

    setup, imports to main module, all-exceptions filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { DatabaseModule } from './database/database.module';
+import { TodosUsingPrismaModule } from './todos-using-prisma/todos-using-prisma.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { CustomLoggerModule } from './custom-logger/custom-logger.module';
 
 @Module({
-  imports: [TodosModule, DatabaseModule],
+  imports: [
+    TodosModule,
+    DatabaseModule,
+    TodosUsingPrismaModule,
+    ThrottlerModule.forRoot([
+      { name: 'short', ttl: 1000, limit: 3 },
+      { name: 'long', ttl: 60000, limit: 100 },
+    ]),
+    CustomLoggerModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
+//You should uncomment commented code to use logger globally
+// import { CustomLoggerModule } from './custom-logger/custom-logger.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    // { bufferLogs: true}
+  );
+  // app.useLogger(app.get(CustomLoggerModule));
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(PORT, () =>
     console.log(`Server is listening on ${PORT} port`),
   );
