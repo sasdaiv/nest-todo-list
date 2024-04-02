e66ce1a2aa4546f6e838cb7f2a74b15fbd27a753
sasdaiv <sasdaiv@gmail.com>
2024-04-02 15:06:34 +0300

    todos service/contoller with mocked data + simple validation

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -4,31 +4,42 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
+  ValidationPipe,
 } from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { FindTodosDto } from './dto/find-all-todos.dto';
 
 @Controller('todos')
 export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
   @Get()
-  findAll(@Query('status') status?: 'active' | 'completed') {
-    return [];
+  findAll(@Query(ValidationPipe) query: FindTodosDto) {
+    return this.todosService.findAll(query.status);
   }
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return id;
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.todosService.findOne(id);
   }
   @Post()
-  create(@Body() todo: {}) {
-    return todo;
+  create(@Body(ValidationPipe) newTodo: CreateTodoDto) {
+    return this.todosService.create(newTodo);
   }
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return id;
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.todosService.remove(id);
   }
   @Patch(':id')
-  update(@Param('id') id: string) {
-    return id;
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updatedTodo: UpdateTodoDto,
+  ) {
+    return this.todosService.update(id, updatedTodo);
   }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,13 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Injectable()
 export class TodosService {
-  private todos = [];
+  private todos = [
+    { id: 1, title: 'Todo 1', status: 'active' },
+    { id: 2, title: 'Todo 2', status: 'active' },
+    { id: 3, title: 'Todo 3', status: 'completed' },
+  ];
   findAll(status?: 'active' | 'completed') {
     if (status) {
       return this.todos.filter((todo) => todo.status === status);
     } else {
       return this.todos;
     }
+  }
+  findOne(id: number) {
+    const todo = this.todos.find((todo) => todo.id === id);
+    if (!todo) {
+      throw new NotFoundException('Todo not found');
+    }
+    return todo;
+  }
+  create(newTodo: CreateTodoDto) {
+    const newTodoWithID = {
+      id: this.todos[this.todos.length - 1].id + 1,
+      ...newTodo,
+    };
+    this.todos.push(newTodoWithID);
+    return newTodo;
+  }
+  update(id: number, todoToUpdate: UpdateTodoDto) {
+    const updatedTodoInDB = this.findOne(id);
+    if (!updatedTodoInDB) {
+      throw new NotFoundException('Todo not found');
+    }
+    const updatedTodo = { ...updatedTodoInDB, ...todoToUpdate };
+    this.todos = this.todos.map((todo) =>
+      todo.id === id ? updatedTodo : todo,
+    );
+    return updatedTodo;
+  }
+  remove(id: number) {
+    const removedUser = this.findOne(id);
+    if (removedUser) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+      return removedUser;
+    }
+    return;
   }
 }
